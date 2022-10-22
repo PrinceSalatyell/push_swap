5050abe5544600b6a21e80380fb19da047fa4d22
princesalatyell <salatyelll14@gmail.com>
2022-10-22 02:18:40 +0100

    added lib content and headers

---
+++ push_swap.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: josanton <josanton@student.42lisboa.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/22 01:53:32 by josanton          #+#    #+#             */
+/*   Updated: 2022/10/22 02:17:55 by josanton         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include "libft/libft.h"
+
+typedef struct s_stack
+{
+	int				value;
+	int				index;
+	int				pos;
+	int				target_pos;
+	int				cost_a;
+	int				cost_b;
+	struct s_stack	*next;
+}	t_stack;
+
+#endif
